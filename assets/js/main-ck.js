f7dfb202d760ddcdedcfb9f7a43e8acadc8e751c
logeshpaul <logesh.paul@sourcebits.com>
2012-11-13 22:19:27 +0530

    Languages & Wishlist dropdown implementation, file restructuring & few other ui improvements

--- assets/js/main-ck.js
+++ assets/js/main-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".carousel").carousel()});
+$(document).ready(function(){$(".carousel").carousel();$("#selectLanguage").click(function(){$(this).toggleClass("active");$(".choose-language").fadeToggle()});$("#watchList").click(function(){$(this).toggleClass("active");$(".watchlist-dropdown").fadeToggle()})});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   $('.carousel').carousel();
+
+  $('#selectLanguage').click(function(){
+  	$(this).toggleClass('active');
+  	$('.choose-language').fadeToggle();
+  });
+
+  $('#watchList').click(function(){
+  	$(this).toggleClass('active');
+  	$('.watchlist-dropdown').fadeToggle();
+  });
 });
